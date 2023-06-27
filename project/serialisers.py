d99c9fb28f1af20cb20820269375ad99b7f186b6
Guillaume <guillaume.oblin@greta-cfa-aquitaine.academy>
2023-06-27 17:14:09 +0200

    creation de la requete de creation de projet

--- project/serialisers.py
+++ project/serialisers.py
@@ -8,16 +8,16 @@ class ProjectSerializer(ModelSerializer):
         model = Project
         fields = ["id", "title", "type", "author_user_id", "staff", "time_created"]
 
-    def get_issues(self, instance):
-        # Le paramètre 'instance' est l'instance de la projet consultée.
-        # Dans le cas d'une liste, cette méthode est appelée autant de fois qu'il y a
-        # d'entités dans la liste
-        # On applique le filtre sur notre queryset pour n'avoir que les probleme actifs
-        queryset = instance.issues.all()
-        # Le serializer est créé avec le queryset défini et toujours défini en tant que many=True
-        serializer = IssuesSerializer(queryset, many=True)
-        # la propriété '.data' est le rendu de notre serializer que nous retournons ici
-        return serializer.data
+        def get_issues(self, instance):
+            # Le paramètre 'instance' est l'instance de la projet consultée.
+            # Dans le cas d'une liste, cette méthode est appelée autant de fois qu'il y a
+            # d'entités dans la liste
+            # On applique le filtre sur notre queryset pour n'avoir que les probleme actifs
+            queryset = instance.issues.all()
+            # Le serializer est créé avec le queryset défini et toujours défini en tant que many=True
+            serializer = IssuesSerializer(queryset, many=True)
+            # la propriété '.data' est le rendu de notre serializer que nous retournons ici
+            return serializer.data
 
 
 class CommentsSerializer(ModelSerializer):
--- project/views.py
+++ project/views.py
@@ -10,8 +10,14 @@ class ProjectViewset(ModelViewSet):
     serializer_class = ProjectSerializer
     # permission_classes = [IsAuthenticated]
 
+    def create(self, request, args, **kwargs):
+        request.POST._mutable = True
+        request.data["author_user_id"] = request.user.pk
+        request.POST._mutable = False
+        return super(ProjectViewset, self).create(request, args, **kwargs)
+
     def get_queryset(self):
         if self.request.user.is_superuser:
             return Project.objects.all()
         else:
-            return Project.objects.filter(staff=self.request.user)
+            return Project.objects.all()
