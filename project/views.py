28e64392651dc87295b9d9b00ab4487dbac384bf
Guillaume <guillaume.oblin@greta-cfa-aquitaine.academy>
2023-06-28 11:55:43 +0200

    creation project ok

--- project/views.py
+++ project/views.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
-
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Project
 from .serialisers import ProjectSerializer
@@ -10,14 +11,18 @@ class ProjectViewset(ModelViewSet):
     serializer_class = ProjectSerializer
     # permission_classes = [IsAuthenticated]
 
-    def create(self, request, args, **kwargs):
+    """def create(self, request, *args, **kwargs):
         request.POST._mutable = True
-        request.data["author_user_id"] = request.user.pk
+        request.data["author_user_id"] = request.user
         request.POST._mutable = False
-        return super(ProjectViewset, self).create(request, args, **kwargs)
+        print(request.data)
+        return super(ProjectViewset, self).create(request, *args, **kwargs)"""
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author_user_id=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def get_queryset(self):
-        if self.request.user.is_superuser:
-            return Project.objects.all()
-        else:
-            return Project.objects.all()
+        return Project.objects.all()
